12a09fc02d65f660f101bbce67cea57ea8f1fd5d
Margaret Chege <ciku@MacBook-Pro-4.local>
2019-03-01 19:45:32 +0300

    feat (read-time) Get read time for articles Create a view for read time of an article Create test for

---
+++ src/contactus.js
@@ -0,0 +1,12 @@
+import React from 'react';
+class contactus extends Component {
+  state = {  }
+  render() { 
+    return ( 
+    <div>
+      <h1>Heeey there</h1>
+    </div> );
+  }
+}
+ 
+export default contactus;
